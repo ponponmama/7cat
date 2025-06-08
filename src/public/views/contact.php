db153febbdf81fec9bd8ea8e2fe2cb6184ff8490
ponponmama <buti1024@gmail.com>
2025-06-08 17:47:43 +0900

    contactページ作成

---
+++ src/public/views/contact.php
@@ -0,0 +1,9 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$page_title = 'Contact';
+$page_css = 'contact.css';
+$content = __DIR__ . '/contact.html';
+include __DIR__ . '/layouts/app.php';
+?>
